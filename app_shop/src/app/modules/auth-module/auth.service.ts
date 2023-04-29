bd1862a752b90f90b0baf10d0c7292f52dac6d52
Oniel_E <oniel.espinal@unah.hn>
2023-04-28 19:39:35 -0600

    clientes listos

--- app_shop/src/app/modules/auth-module/auth.service.ts
+++ app_shop/src/app/modules/auth-module/auth.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private apiUrl = 'http://tu-dominio.com/api-token-auth/';
 
-  constructor() { }
-}
+  constructor(private http: HttpClient) { }
+
+  login(email: string, password: string) {
+    return this.http.post(this.apiUrl, {email: email, password: password});
+  }
+} 
